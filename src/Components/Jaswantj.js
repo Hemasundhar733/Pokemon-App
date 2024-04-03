859d9e12cfb7770c907260ea6ecc843592125b91
Hema sundhar Reddy <69297895+Hemasundhar733@users.noreply.github.com>
2024-04-04 01:39:16 +0530

    Initial commit

---
+++ src/Components/Jaswantj.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Jaswantj() {
+  return (
+    <div>
+        "Iam jaswanth"
+    </div>
+  )
+}
+
+export default Jaswantj;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Jaswantj from './Components/Jaswantj'; 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* <Jaswantj/> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
